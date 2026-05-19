d385d78a40bf81f4148028db7ee8135e64767a0e
lyog <victordamascenos2015@gmail.com>
2026-05-19 19:33:23 -0300

    Primeiro commit

---
+++ src/main/java/filehub/com/example/FilehubApplication.java
@@ -0,0 +1,13 @@
+package filehub.com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilehubApplication.class, args);
+	}
+
+}
